c0503f19d9d4fde30322071faaeae36813af8048
Michael Kiros <michael.kiros00@gmail.com>
2022-12-08 20:42:01 -0500

    commit tree d47ba853ca87eba43047ebc8b18c6f13da16263d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, event) { 
+   const newArray = [] 
+   for (let i = 0; i < names.length; i++) {
+   newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+   }
+   return newArray
+}
+function countDown(value) {
+    while (value > -1)
+    console.log(value--)
+}
